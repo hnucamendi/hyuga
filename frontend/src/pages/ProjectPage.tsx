c2751774a3b64abe2194ead057dbf0a444394359
hnucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-08-21 06:58:46 -0400

    save

--- frontend/src/pages/ProjectPage.tsx
+++ frontend/src/pages/ProjectPage.tsx
@@ -1,64 +1,73 @@
-import Header from "../components/Header"
-import { useParams } from "react-router-dom"
+import Header from "../components/Header";
+import { useParams } from "react-router-dom";
 import type { main } from "../../wailsjs/go/models";
-import { useEffect, useRef, useState } from "react"
-import { LoadProject, UploadPhoto } from "../../wailsjs/go/main/App"
+import { useEffect, useRef, useState } from "react";
+import { LoadProject, UploadPhoto } from "../../wailsjs/go/main/App";
 import Aside from "../components/Aside";
 import Button from "../components/Button";
 
 function ProjectPage() {
-  const { projectId } = useParams<{ projectId: string }>()
-  const [project, setProject] = useState<main.Project>()
-  const attributeId = useRef(0)
+  const { projectId } = useParams<{ projectId: string }>();
+  const [project, setProject] = useState<main.Project>();
+  const attributeId = useRef(0);
 
   useEffect(() => {
     const fetchProject = async () => {
-      const proj = await LoadProject(projectId || "")
-      setProject(proj)
-    }
-    fetchProject()
-  })
+      const proj = await LoadProject(projectId || "");
+      setProject(proj);
+    };
+    fetchProject();
+  });
 
-  const handleAddMetadataPacket = () => {
-  }
+  const handleAddMetadataPacket = () => {};
 
   const handleUpload = async (type: string) => {
-    if (!projectId) return
-    const input = document.createElement("input")
-    input.type = "file"
-    input.accept = "image/*"
+    if (!projectId) return;
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
 
     input.onchange = async () => {
-      const file = input.files?.[0]
-      if (!file) return
+      const file = input.files?.[0];
+      if (!file) return;
 
-      const reader = new FileReader()
+      const reader = new FileReader();
       reader.onload = async () => {
-        const base64 = reader.result?.toString().split(",")[1] // remove data:image/... prefix
-        if (!base64) return
+        const base64 = reader.result?.toString().split(",")[1]; // remove data:image/... prefix
+        if (!base64) return;
 
         try {
           switch (type) {
             case "sheet": {
-              await UploadPhoto(base64, "sheet", projectId, attributeId.current) // send base64 and custom type to backend
-              break
+              await UploadPhoto(
+                base64,
+                "sheet",
+                projectId,
+                attributeId.current.toString(),
+              ); // send base64 and custom type to backend
+              break;
             }
             case "cutout": {
-              await UploadPhoto(base64, "cutout", projectId, attributeId.current) // send base64 and custom type to backend
-              break
+              await UploadPhoto(
+                base64,
+                "cutout",
+                projectId,
+                attributeId.current.toString(),
+              ); // send base64 and custom type to backend
+              break;
             }
           }
         } catch (err) {
-          console.error("Upload failed", err)
+          console.error("Upload failed", err);
         }
-      }
-      reader.readAsDataURL(file)
-    }
-    input.click()
-  }
+      };
+      reader.readAsDataURL(file);
+    };
+    input.click();
+  };
 
   if (!projectId) {
-    return <div>No project ID found.</div>
+    return <div>No project ID found.</div>;
   }
 
   return (
@@ -74,13 +83,9 @@ function ProjectPage() {
         label="Añadir foto de Nota"
         onClick={() => handleUpload("cutout")}
       />
-      <Button
-        label="Añadir"
-        onClick={handleAddMetadataPacket}
-      />
-
+      <Button label="Añadir" onClick={handleAddMetadataPacket} />
     </>
-  )
+  );
 }
 
-export default ProjectPage
+export default ProjectPage;
